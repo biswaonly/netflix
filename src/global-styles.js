fd1e8be8cefab45aa88fcc0a6d37c38298807a49
Biswajit Barman <barman@devskool.com>
2021-01-06 13:37:27 +0530

    inital commit

---
+++ src/global-styles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  html,
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    background-color: black;
+    color: #333;
+    font-size: 16px;
+  }
+`;
